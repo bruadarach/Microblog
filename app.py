e20853935fbc8f3fac27266e8e744d180d76412e
bruadarach <55401378+bruadarach@users.noreply.github.com>
2021-07-05 05:39:11 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import datetime
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient("mongodb+srv://main-manager_01:suji@microblog-application.jsn6e.mongodb.net/test")
+    app.db = client.microblog # client.databasename
+
+    # entries = []
+
+    @app.route("/", methods=["GET","POST"])
+    def home():
+        #print([i for i in app.db.entries.find({})]) # print everything stored or that we found in our database
+        
+        if request.method == "POST":
+            entry_content = request.form.get("content") # <textarea name="content">
+            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+            #entries.append((entry_content, formatted_date))
+            app.db.entries.insert({"content": entry_content, "date": formatted_date})
+
+
+        entries_with_date = [
+            # (entry[0], 
+            # entry[1], 
+            # datetime.datetime.strptime(entry[1],"%Y-%m-%d").strftime("%b %d")
+            # )
+            # for entry in entries # This is the query of our db 'entries' access
+
+            (
+                entry["content"],
+                entry["date"],
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for entry in app.db.entries.find({})
+        ]
+
+        return render_template("home.html", entries=entries_with_date)
+
+    return app
